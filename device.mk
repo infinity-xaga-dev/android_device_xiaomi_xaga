69f25f9bbb9685b0117dabd9a1dedf2b7199301a
Tuan Anh <dinhtuananh837@gmail.com>
2026-03-02 13:33:33 +0700

    xaga: Inherit Dolby
    Signed-off-by: Tuan Anh <dinhtuananh837@gmail.com>

--- device.mk
+++ device.mk
@@ -7,6 +7,9 @@
 # Inherit from mt6895-common
 $(call inherit-product, device/xiaomi/mt6895-common/mt6895.mk)
 
+# Inherit Dolby
+$(call inherit-product, hardware/dolby/dolby.mk)
+
 # Call the MiuiCamera setup
 $(call inherit-product-if-exists, vendor/xiaomi/miuicamera-xaga/device.mk)
 
